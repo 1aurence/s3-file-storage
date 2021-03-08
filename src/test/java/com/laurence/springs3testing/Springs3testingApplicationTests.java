6ff26ad8b18d350512cb3fd3b4b152dfd2d0209d
laurence <laurencehall1@outlook.com>
2021-03-08 18:32:13 -0500

    initial

---
+++ src/test/java/com/laurence/springs3testing/Springs3testingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurence.springs3testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springs3testingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
